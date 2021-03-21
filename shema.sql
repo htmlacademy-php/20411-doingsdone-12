5790bf1752c331b7b8498b0a642216fdc83e6d04
nevalashka <b.rukavishnikov@gmail.com>
2021-03-21 07:40:48 +0300

    схема бд 4.1

--- shema.sql
+++ shema.sql
@@ -7,7 +7,7 @@ USE doings-db;
 CREATE TABLE users (
   id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   email char(64) NOT NULL,
-  password char(64) NOT NULL,
+  password char(255) NOT NULL,
   name char(64) NOT NULL,
   date_registration TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
@@ -15,17 +15,17 @@ CREATE TABLE users (
 CREATE TABLE projects (
   id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   user_id int(11) NOT NULL,
-  project char(128) DEFAULT NULL,
+  project_name char(128) DEFAULT NULL,
   FOREIGN KEY (user_id)  REFERENCES users(id)
 );
 
-CREATE TABLE IF NOT EXISTS `tasks` (
+CREATE TABLE IF NOT EXISTS tasks (
   id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   project_id int(11) NOT NULL,
   date_creation TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP,
   status boolean NOT NULL DEFAULT 0,
   task_name char(64) NOT NULL,
-  file_url varchar(100) DEFAULT NULL,
+  file_url varchar(255) DEFAULT NULL,
   date_of_task TIMESTAMP(0) DEFAULT NULL,
   FOREIGN KEY (project_id)  REFERENCES projects(id)
 );
