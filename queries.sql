f88b5c1730407ee67982d3664a660273c74cba5d
nevalashka <b.rukavishnikov@gmail.com>
2021-05-16 12:31:00 +0300

    формирование бд 4.2

--- queries.sql
+++ queries.sql
@@ -3,20 +3,20 @@ INSERT INTO users (email, password, name, date_registration) VALUES
 ('petya@mail.ru', 'qwerty', 'bulochka', 11.05.2015),
 ('user@mail.ru', 'flat-surv-28195', 'piska', 01.10.2017);
 
-INSERT INTO projects (id, user_id, project_name) VALUES
-(1, 2, 'Работа'),
-(2, 1, 'Учеба'),
-(3, 2, 'Входящие'),
-(4, 2, 'Домашние дела'),
-(5, 3, 'Авто');
+INSERT INTO projects (user_id, project_name) VALUES
+(2, 'Работа'),
+(1, 'Учеба'),
+(2, 'Входящие'),
+(2, 'Домашние дела'),
+(3, 'Авто');
 
-INSERT INTO tasks (id, user_id, project_id, date_creation, status, task_name, date_of_implementation) VALUES
-(1, 2, 1, 03.05.2021, false, 'Собеседование в IT компании', 19.02.2021),
-(2, 1, 1, 07.05.2021, false, 'Выполнить тестовое задание', 15.02.2021),
-(3, 1, 2, 08.05.2021, true, 'Сделать задание первого раздела', 14.02.2021),
-(4, 1, 3, 09.05.2021, false, 'Встреча с другом', 16.02.2021),
-(5, 1, 4, 09.05.2021, false, 'Купить корм для кота', 16.02.2021),
-(6, 1, 4, 09.05.2021, false, 'Заказать пиццу', null);
+INSERT INTO tasks (user_id, project_id, date_creation, status, task_name, date_of_implementation) VALUES
+(2, 1, 03.05.2021, false, 'Собеседование в IT компании', 19.02.2021),
+(1, 1, 07.05.2021, false, 'Выполнить тестовое задание', 15.02.2021),
+(1, 2, 08.05.2021, true, 'Сделать задание первого раздела', 14.02.2021),
+(1, 3, 09.05.2021, false, 'Встреча с другом', 16.02.2021),
+(1, 4, 09.05.2021, false, 'Купить корм для кота', 16.02.2021),
+(1, 4, 09.05.2021, false, 'Заказать пиццу', null);
 
 SELECT project_name FROM projects WHERE user_id = 2;
 
