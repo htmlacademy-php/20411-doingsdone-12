61bcacd505024201d74b34a836524f3805ecb7a9
nevalashka <b.rukavishnikov@gmail.com>
2021-08-28 21:48:26 +0300

    выполнение задания 6.1

--- templates/main.php
+++ templates/main.php
@@ -20,7 +20,7 @@
                 <ul class="main-navigation__list">
                     <?php foreach ($projects as $key => $value): ?>
                     <li class="main-navigation__list-item">
-                        <a class="main-navigation__list-item-link" href="<?=esc($value["/?="."project_name"])?>">
+                        <a class="main-navigation__list-item-link" href="<?="/?=".esc($value["project_name"])?>">
                             <?=esc($value['project_name'])?>
                         </a>
                         <span class="main-navigation__list-item-count">
