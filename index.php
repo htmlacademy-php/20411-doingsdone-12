379a475d40f1ef56bc6a05551bcc3647318a26bc
nevalashka <b.rukavishnikov@gmail.com>
2021-08-28 10:40:17 +0300

    выполнение задания 6.1

--- index.php
+++ index.php
@@ -31,46 +31,6 @@ else {
 }
 
 $show_complete_tasks = rand(0, 1);
-/*
-$tasks = [
-    [
-        'name' => 'Собеседование в IT компании',
-        'date_of_implementation' => '19.02.2021',
-        'project' => 'Работа',
-        'done' => false
-    ],
-    [
-        'name' => 'Выполнить тестовое задание',
-        'date_of_implementation' => '15.02.2021',
-        'project' => 'Работа',
-        'done' => false
-    ],
-    [
-        'name' => 'Сделать задание первого раздела',
-        'date_of_implementation' => '14.02.2021',
-        'project' => 'Учеба',
-        'done' => true
-    ],
-    [
-        'name' => 'Встреча с другом',
-        'date_of_implementation' => '16.02.2021',
-        'project' => 'Входящие',
-        'done' => false
-    ],
-    [
-        'name' => 'Купить корм для кота',
-        'date_of_implementation' => '17.02.2021',
-        'project' => 'Домашние дела',
-        'done' =>  false
-    ],
-    [
-        'name' => 'Заказать пиццу',
-        'date_of_implementation' => null,
-        'project' => 'Домашние дела',
-        'done' => false
-    ]
-];
-*/
 
 $page_content = include_template('main.php',
                                  ['tasks' => $tasks,
