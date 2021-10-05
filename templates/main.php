1a5a435d2a90ef55309d6d78fa1b883f30083d91
nevalashka <b.rukavishnikov@gmail.com>
2021-10-05 21:54:57 +0300

    выполнение задания 6.1

--- templates/main.php
+++ templates/main.php
@@ -28,7 +28,7 @@
                             <?=esc($value['project_name'])?>
                         </a>
                         <span class="main-navigation__list-item-count">
-                            <?= count_projects ($tasks, $value);?>
+                            <?= count_projects ($tasks, $value); var_dump($value);?>
                         </span>
                     </li>
                     <?php endforeach; ?>
